1dcc337bb1122ed2cb3a9d43c27db6e83ff4186f
strobie <0xstrobe@protonmail.com>
2022-09-22 13:37:31 +0200

    feat: use new icons for detector

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,8 +1,13 @@
+console.log("background loaded");
+
 import checkForPhishing from "eth-phishing-detect";
 import gib from "@assets/img/memes/gib-128.png";
 import maxPain from "@assets/img/memes/max-pain-128.png";
+import que from "@assets/img/memes/que-128.png";
+import upOnly from "@assets/img/memes/up-only-128.png";
 
-console.log("background loaded");
+import PhishingDetector from "eth-phishing-detect/src/detector";
+import detectorConfig from "eth-phishing-detect/src/config.json";
 
 async function getCurrentTab() {
   const queryOptions = { active: true, currentWindow: true };
@@ -11,27 +16,29 @@ async function getCurrentTab() {
 }
 
 async function handlePhishingCheck() {
+  const detector = new PhishingDetector(detectorConfig);
+
   let isPhishing = false;
   const tab = await getCurrentTab();
-  const url = tab.url;
-
-  if (url.startsWith("https://metamask.github.io/phishing-warning")) {
-    // already captured and redirected to metamask phishing warning page
-    isPhishing = true;
-  } else {
-    try {
+  try {
+    const url = tab.url;
+    if (url.startsWith("https://metamask.github.io/phishing-warning")) {
+      // already captured and redirected to metamask phishing warning page
+      isPhishing = true;
+    } else {
       const domain = new URL(url).hostname.replace("www.", "");
       isPhishing = checkForPhishing(domain);
-    } catch (error) {
-      // ignore error incase of invalid url, just treat as non-phishing
-      console.error(error);
+      console.log("detector", detector.check(domain));
     }
+  } catch (error) {
+    // ignore error incase of invalid url, just treat as non-phishing
+    console.log(error);
   }
 
   if (isPhishing) {
     chrome.action.setIcon({ path: maxPain });
   } else {
-    chrome.action.setIcon({ path: gib });
+    chrome.action.setIcon({ path: que });
   }
 
   return isPhishing;
