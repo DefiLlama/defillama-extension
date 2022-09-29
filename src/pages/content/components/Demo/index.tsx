05c9bdb658bf05ce941ed64b21e446033ff3a917
strobie <0xstrobe@protonmail.com>
2022-09-30 00:17:23 +0200

    chore: clean up content

--- src/pages/content/components/Demo/index.tsx
+++
@@ -1,8 +0,0 @@
-import App from "@src/pages/content/components/Demo/app";
-import { createRoot } from "react-dom/client";
-
-const root = document.createElement("div");
-root.id = "defillama-extension-content-view-root";
-document.body.append(root);
-
-createRoot(root).render(<App />);
