ae8739b434412e56f221d87d8fdc32c82e1005c1
strobie <0xstrobe@protonmail.com>
2022-09-29 20:42:49 +0200

    np

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,4 +1,5 @@
 import App from "@src/pages/content/components/Demo/app";
+import { createRoot } from "react-dom/client";
 
 const root = document.createElement("div");
 root.id = "defillama-extension-content-view-root";
