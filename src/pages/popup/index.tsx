b5d7c7f795ad3e85b4478a5951e781847e7f8888
strobie <0xstrobe@protonmail.com>
2022-09-29 22:31:08 +0200

    k

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
-import Popup from "./Popup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Popup from "./Popup";
+import { coinsDb, protocolsDb } from "../libs/db";
 
 const queryClient = new QueryClient();
 
@@ -16,18 +17,19 @@ const theme = extendTheme({
   config,
 });
 
-function init() {
-  const rootElement = document.querySelector("body");
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={theme}>
-          <ColorModeScript />
-          <Popup />
-        </ChakraProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
-}
+const rootElement = document.querySelector("body");
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <Popup />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
 
-init();
+//@ts-ignore
+window.coinsDb = coinsDb;
+//@ts-ignore
+window.protocols = protocolsDb;
