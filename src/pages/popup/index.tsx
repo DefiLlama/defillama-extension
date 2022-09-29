6ea85580cfb6d512035d3c6a909144dc5da2220d
strobie <0xstrobe@protonmail.com>
2022-09-29 22:29:30 +0200

    color stuff

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,17 +1,32 @@
-import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
 import Popup from "./Popup";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+  disableTransitionOnChange: false,
+};
+
+const theme = extendTheme({
+  config,
+});
 
 function init() {
-  const appContainer = document.querySelector("body");
-  if (!appContainer) {
-    throw new Error("Can not find AppContainer");
-  }
-  const root = createRoot(appContainer);
-  root.render(
-    <ChakraProvider>
-      <Popup />
-    </ChakraProvider>,
+  const rootElement = document.querySelector("body");
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript />
+          <Popup />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </React.StrictMode>,
   );
 }
 
