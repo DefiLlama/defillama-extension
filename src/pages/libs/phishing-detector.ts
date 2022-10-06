269e644bacca31892960cb9cb9b60c79045ddbc3
strobie <0xstrobe@protonmail.com>
2022-10-06 14:42:58 +0200

    oke

--- src/pages/libs/phishing-detector.ts
+++ src/pages/libs/phishing-detector.ts
@@ -10,29 +10,40 @@ interface CheckDomainResult {
 }
 
 export async function checkDomain(domain: string): Promise<CheckDomainResult> {
-  const isAllowed = await allowedDomainsDb.domains.get({ domain });
+  console.log("Checking domain", domain);
+  const topLevelDomain = domain.split(".").slice(-2).join(".");
+  const isAllowed =
+    (await allowedDomainsDb.domains.get({ domain })) ||
+    (await allowedDomainsDb.domains.get({ domain: topLevelDomain }));
   if (isAllowed) {
     return { result: false, type: "allowed" };
   }
 
-  const isBlocked = await blockedDomainsDb.domains.get({ domain });
+  const isBlocked =
+    (await blockedDomainsDb.domains.get({ domain })) ||
+    (await blockedDomainsDb.domains.get({ domain: topLevelDomain }));
   if (isBlocked) {
     return { result: true, type: "blocked" };
   }
 
   const fuzzyDomains = fuzzyDomainsDb.domains.toCollection();
-  const match = await fuzzyDomains.first((x) => {
-    const distance = levenshtein.get(x.domain, domain);
-    const isMatched = distance <= DEFAULT_LEVENSHTEIN_TOLERANCE;
+  let fuzzyResult: CheckDomainResult;
+  await fuzzyDomains
+    .until((x) => {
+      const distance = levenshtein.get(x.domain, domain);
+      const distanceTop = levenshtein.get(x.domain, topLevelDomain);
+      const isMatched = distance <= DEFAULT_LEVENSHTEIN_TOLERANCE || distanceTop <= DEFAULT_LEVENSHTEIN_TOLERANCE;
 
-    if (isMatched) {
-      console.log("fuzzy match", { domain, fuzzyDomain: x.domain, distance });
-      return { result: true, type: "fuzzy", extra: x.domain } as const;
-    }
-  });
+      if (isMatched) {
+        console.log("fuzzy match", { domain, fuzzyDomain: x.domain, distance });
+        fuzzyResult = { result: true, type: "fuzzy", extra: x.domain };
+        return true;
+      }
+    }, true)
+    .last();
 
-  if (match) {
-    return match;
+  if (fuzzyResult) {
+    return fuzzyResult;
   }
 
   return { result: false, type: "unknown" };
