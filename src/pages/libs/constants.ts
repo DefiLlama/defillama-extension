fa0abbb434c573275264f5b364e016056ad4f2e7
strobie <0xstrobe@protonmail.com>
2022-11-05 12:19:12 +0100

    add types

--- src/pages/libs/constants.ts
+++ src/pages/libs/constants.ts
@@ -14,6 +14,11 @@ export const PROTOCOLS_API = "https://api.llama.fi/lite/protocols2";
 export const PROTOCOL_TVL_THRESHOLD = 1000000;
 
 export const ACCOUNTS_API = "https://cutest.llamao.fi/api/v0/address/";
+export interface TagsData {
+  address: string; // with 0x prefix
+  tags: string[];
+}
+export type AccountsResponse = TagsData[];
 
 export const METAMASK_LIST_CONFIG_API =
   "https://raw.githubusercontent.com/MetaMask/eth-phishing-detect/master/src/config.json";
