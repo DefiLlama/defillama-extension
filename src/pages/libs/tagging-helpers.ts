08f64800b46efbd546822d439bbd34967f92a0dc
strobie <0xstrobe@protonmail.com>
2022-11-05 15:48:58 +0100

    initial stuff for displaying tags

--- src/pages/libs/tagging-helpers.ts
+++ src/pages/libs/tagging-helpers.ts
@@ -1,29 +1,58 @@
+import { TagsData } from "./constants";
 import llub from "@src/assets/img/memes/llub-128.png";
 import smort from "@src/assets/img/memes/smort-128.png";
 import institute from "@src/assets/img/memes/institute-128.png";
-import { TagsData } from "./constants";
+import opensea from "@src/assets/img/protocols/opensea.png";
+import uniswap from "@src/assets/img/protocols/uniswap.png";
 
 export interface DisplayTag {
   name: string;
   color?: string;
   description?: string;
   icon?: string;
-  link?: string;
+  link?: (input: string) => string;
 }
-
-const makeDisplayTags = (res: TagsData) => {
-  const tags: DisplayTag[] = [];
-  const rawTags = res.tags;
-  for (const rawTag of rawTags) {
-  }
-};
 
 const TAG_TYPES = {
   OPENSEA: {
-    icon: "opensea",
+    icon: opensea,
     link: (input: string) => `https://opensea.io/${input}`,
   },
-  UNISWAP: {
-    icon: "uniswap",
-  },
+  // UNISWAP: {
+  //   icon: uniswap,
+  // },
 } as const;
+
+export const makeDisplayTags = (res: TagsData) => {
+  const tags: DisplayTag[] = [];
+  const rawTags = res.tags;
+
+  if (rawTags.includes("OpenSea User")) {
+    // if doesn't have another tag starting with "OpenSea:", add "OpenSea User" tag.
+    // otherwise, add the tag starting with "OpenSea:" tag.
+    const openSeaTag = rawTags.find((tag) => tag.startsWith("OpenSea: "));
+    if (openSeaTag) {
+      tags.push({
+        name: openSeaTag.replace("OpenSea: ", ""),
+        description: "OpenSea User",
+        ...TAG_TYPES.OPENSEA,
+      });
+    } else {
+      tags.push({
+        name: "OpenSea User",
+        ...TAG_TYPES.OPENSEA,
+      });
+    }
+  }
+
+  for (const rawTag of rawTags) {
+    if (rawTag.startsWith("OpenSea: ") || rawTag === "OpenSea User") {
+      continue;
+    }
+    tags.push({
+      name: rawTag,
+    });
+  }
+
+  return tags;
+};
