329b467743209acbd50ad0406a947432606154af
strobie <0xstrobe@protonmail.com>
2023-02-21 01:58:43 +0100

    add back injectTags

--- src/pages/content/components/explorers/ethereum.tsx
+++ src/pages/content/components/explorers/ethereum.tsx
@@ -1,12 +1,5 @@
-// import { EtherscanAlikeExplorerConfig } from "../etherscanInjectPrice";
 import { injectPrice } from "../new/injectPrice";
-
-// const config: EtherscanAlikeExplorerConfig = {
-//   name: "Etherscan",
-//   indexTotalAmountTextSplit: 1,
-//   selectorTokenList: "li.list-custom.list-custom-ERC20 > a",
-//   chainPrefix: "ethereum:",
-// };
+import { injectTags } from "../new/injectTags";
 
 injectPrice();
-// injectTags();
+injectTags();
