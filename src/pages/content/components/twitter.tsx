8f9928d317e027ae9e27b32bfbb2054cc012cd06
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-01-30 15:53:09 +0100

    Refactor Twitter handle verification and add debounce functionality

--- src/pages/content/components/twitter.tsx
+++ src/pages/content/components/twitter.tsx
@@ -1,7 +1,8 @@
 import { getStorage, } from "@src/pages/libs/helpers";
 
 initPhishingHandleDetector();
-const debouncedVerifyHandle = debounce(verifyHandle, 200);
+const debouncedVerifyHandle = debounce(verifyHandle, 200, );
+const debouncedVerifyHandle2 = debounce(verifyHandle, 5000); // maybe tweets take some time to load if you scroll too fast
 
 
 async function initPhishingHandleDetector() {
@@ -11,27 +12,47 @@ async function initPhishingHandleDetector() {
   verifyHandle();
   window.addEventListener('scroll', () => {
     debouncedVerifyHandle();
+    debouncedVerifyHandle2();
   });
 }
+
+const susHandles = new Set() as Set<string>;
 
 async function verifyHandle() {
   const isTweetPage = window.location.pathname.split("/")[2] === "status"
   if (!isTweetPage) return;
 
   const safeHandle = window.location.pathname.split("/")[1]
+  let safeDisplayName
   const tweets = document.querySelectorAll('[data-testid="tweet"]')
   for (const tweet of tweets) {
-    const tweetHandle = (tweet.querySelector('a[role="link"]') as any).href.replace(/.*twitter.com\//, '')
-    if (areHandlesSimilar(safeHandle, tweetHandle)) {
-      (tweet as any).style.background = '#ff000069' // set background as light red
-    }
+    const element = tweet.querySelectorAll('a[role="link"]')
+    const tweetHandle = (element[2] as any).innerText.replace('@', '')
+    const displayName = (element[1] as any).innerText
+
+    if (susHandles.has(tweetHandle))
+      handleSusTweets(tweet, tweetHandle)
+    else if (tweetHandle === safeHandle)
+      safeDisplayName = displayName
+    else if (safeDisplayName && areHandlesSimilar(safeDisplayName, displayName, 3))
+      handleSusTweets(tweet, tweetHandle)
+    else if (areHandlesSimilar(safeHandle, tweetHandle))
+      handleSusTweets(tweet, tweetHandle)
+
+  }
+
+  function handleSusTweets(tweet: any, handle: string) {
+    susHandles.add(handle);
+    (tweet as any).style.background = '#ff000069' // set background as light red
+
   }
 }
 
 function areHandlesSimilar(handle1, handle2, threshold = 4) {
-  if (handle1 === handle2) return false;
   if (handle1.length > handle2.length) handle1 = handle1.slice(0, handle2.length);
-  else if (handle2.length > handle1.length) handle2 = handle2.slice(0, handle1.length);
+  if (handle2.length > handle1.length) handle2 = handle2.slice(0, handle1.length);
+
+  if (handle1 === handle2) return true;
   const distance = levenshteinDistance(handle1, handle2);
   return distance <= threshold; // or whatever threshold you consider as "similar"
 
@@ -65,13 +86,13 @@ function areHandlesSimilar(handle1, handle2, threshold = 4) {
   }
 }
 
-let debounceTimer;
+const debounceTimers = {} as any
 
 function debounce(func, delay) {
   return function () {
     const context = this;
     const args = arguments;
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    clearTimeout(debounceTimers[delay]);
+    debounceTimers[delay] = setTimeout(() => func.apply(context, args), delay);
   }
 }
