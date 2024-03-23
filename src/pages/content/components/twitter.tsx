ba90cd30dd94742c91130e54b2cf0eb2760ed944
ChainKhan <thecryptokhan@gmail.com>
2024-03-23 16:06:43 -0400

    cleaned up debouncing. Added ad tweet hiding

--- src/pages/content/components/twitter.tsx
+++ src/pages/content/components/twitter.tsx
@@ -2,23 +2,33 @@ import { getStorage } from "@src/pages/libs/helpers";
 import levenshtein from "fast-levenshtein";
 
 initPhishingHandleDetector();
-const debouncedVerifyHandleOnTweetPage = debounce(verifyHandleOnTweetPage, 200);
-const debouncedVerifyHandleOnTweetPage2 = debounce(verifyHandleOnTweetPage, 2000); // maybe tweets take some time to load if you scroll too fast
-const debouncedVerifyHandleOnTweetPage3 = debounce(verifyHandleOnTweetPage, 5000); // maybe tweets take some time to load if you scroll too fast
 
 async function initPhishingHandleDetector() {
   const phishingHandleDetector = await getStorage("local", "settings:phishingHandleDetector", true);
   if (!phishingHandleDetector) return;
 
-  verifyHandleOnTweetPage();
+  const handlePage = getHandlerForTwitterPageVariant();
+  handlePage(); // initial run on load
+
   window.addEventListener("scroll", () => {
-    debouncedVerifyHandleOnTweetPage();
-    debouncedVerifyHandleOnTweetPage2();
-    debouncedVerifyHandleOnTweetPage3();
+    debounce(handlePage, 200)();
+    // consecutive debounces in case of tweets loading in slower on fast scrolling
+    debounce(handlePage, 2000)();
+    debounce(handlePage, 5000)();
   });
 }
 
+// type TwitterPageVariant = "home" | "status";
+
 //
+
+function getHandlerForTwitterPageVariant() {
+  const pathname = window.location.pathname;
+  console.log("dl_dev", window.location.pathname, pathname.split("/")[2] === "status", pathname === "/home");
+
+  if (pathname.split("/")[2] === "status") return verifyHandleOnTweetPage;
+  else if (pathname === "/home") handleHomePage;
+}
 
 const handleToName = {} as Record<string, string>;
 async function verifyHandleOnTweetPage() {
@@ -34,6 +44,8 @@ async function verifyHandleOnTweetPage() {
 
   tweets.forEach((tweet, index) => {
     const { tweetHandle, displayName, tweetText, isRepliedTo } = getTweetInfo(tweet);
+
+    handleAdTweet(tweet);
 
     // if the tweet text content consists of only numbers, then it's sus. Add red background the tweet
     if (/^[0-9]+$/.test(tweetText)) {
@@ -67,11 +79,43 @@ async function verifyHandleOnTweetPage() {
 
 function handleSusTweet(tweet: HTMLElement, action: "BG_RED" | "HIDE") {
   if (action === "BG_RED") tweet.style.background = "#ff000069"; // set background as light red
-  // else if (action === "yellow")
 }
 
-async function handleHomePage(twitterConfig) {
-  return; // disable for now
+/**
+ * Hides the tweet if it's an ad.
+ * Determined by the presence of the "Ad" text on the top right corner of the tweet.
+ */
+function handleAdTweet(tweet: HTMLElement) {
+  // get Ad span element on top right corner of tweet directly via xpath from tweet article element
+  const xpathArticleToAdText = "div/div/div[2]/div[2]/div[1]/div/div[2]/div/div[1]/span";
+  const adIndicator = document.evaluate(
+    xpathArticleToAdText,
+    tweet,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null,
+  ).singleNodeValue;
+  if (!adIndicator) return;
+
+  console.log(adIndicator.textContent, adIndicator.textContent === "Ad");
+
+  // hide the ad tweet
+  if (adIndicator.textContent === "Ad") {
+    tweet.style.display = "none";
+  }
+}
+
+async function handleHomePage(/* twitterConfig */) {
+  const isHomePage = window.location.pathname.split("/")[2] === "status";
+  if (!isHomePage) return;
+
+  const tweets = Array.from(document.querySelectorAll<HTMLElement>('[data-testid="tweet"]'));
+
+  tweets.forEach((tweet, index) => {
+    handleAdTweet(tweet);
+  });
+
+  // return; // disable for now
   /*
   const tweets = document.querySelectorAll('[data-testid="tweet"]');
   for (const tweet of tweets) {
@@ -95,7 +139,7 @@ function getTweetInfo(tweet: HTMLElement) {
     return +element.getAttribute("aria-label").split(" ")[0];
   };
 
-  let element = Array.from(tweet.querySelectorAll<HTMLElement>('a[role="link"]'));
+  let element = Array.from(tweet.querySelectorAll<HTMLElement>('a[role="link"]')); //? better error handling: check for .innerText
   if (element[0].innerText.endsWith("retweeted") || element[0].innerText.endsWith("reposted"))
     element = Array.from(element).slice(1);
 
