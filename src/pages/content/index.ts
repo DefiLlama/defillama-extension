7c7265efff736d29e537f688c47e20f377d6cf68
ChainKhan <thecryptokhan@gmail.com>
2024-03-27 22:53:15 -0400

    changed twitter content script routing

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -24,7 +24,7 @@ switch (new URL(document.baseURI).hostname) {
     break;
   case "twitter.com":
   case "x.com":
-    import("./components/twitter");
+    import("./components/twitter/init");
     break;
   default:
     break;
