669f87cac624bc7ea6580a0b3448c40b06067e9e
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-02-17 09:38:19 +0100

    Refactor explorer component to use a centralized prefix map for chain prefixes

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,34 +1,18 @@
+import { EXPLORER_CHAIN_PREFIX_MAP } from "../libs/constants";
 import genericEtherscanComponent from "./components/explorers/genericEtherscanComponent"
 import initPhishingDetector from  "./components/twitter/init"
 
-switch (new URL(document.baseURI).hostname) {
-  case "etherscan.io":
-  case "bscscan.com":
-  case "ftmscan.com":
-  case "arbiscan.io":
-  case "polygonscan.com":
-  case "optimistic.etherscan.io":
-  case "cronoscan.com":
-  case "moonriver.moonscan.io":
-  case "moonbeam.moonscan.io":
-  case "moonscan.io":
-  case "gnosisscan.io":
-  case "bobascan.com":
-  case "zkevm.polygonscan.com":
-  case "nova.arbiscan.io":
-  case "celoscan.com":
-  case "bttcscan.com":
-  case "scrollscan.com":
-  case "lineascan.build":
-  case "basescan.org":
-  case "era.zksync.network":
-  case "kromascan.com":
-    genericEtherscanComponent();
-    break;
-  case "twitter.com":
-  case "x.com":
-    initPhishingDetector();
-    break;
-  default:
-    break;
+const host = new URL(document.baseURI).hostname;
+
+if (EXPLORER_CHAIN_PREFIX_MAP[host]) {
+  genericEtherscanComponent();
+} else {
+  switch (host) {
+    case "twitter.com":
+    case "x.com":
+      initPhishingDetector();
+      break;
+    default:
+      break;
+  }
 }
